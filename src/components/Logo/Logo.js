b5a7ce0e7bb9a6350a63331ce12d64b32e62379e
Horea Tiff <horea.tiff@gmail.com>
2022-11-19 11:15:54 +0200

    Added components

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,24 +1,19 @@
-import React from "react";
-import "./Logo.css";
-import brain from "./brain.png";
-import Tilt from "react-parallax-tilt";
+import React from 'react';
+// import Tilt from 'react-tilt'; 
+import Tilt from 'react-parallax-tilt';
+import brain from './brain.png';
+import './Logo.css';
 
 const Logo = () => {
   return (
-    <div className="ma4 mt0">
-      <Tilt
-        className="Tilt"
-        br2
-        shadow-2
-        options={{ max: 25 }}
-        style={{ height: 150, width: 150, borderRadius: "10px" }}
-      >
+    <div className='ma4 mt0'>
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
         <div className="Tilt-inner pa3">
-          <img style={{ paddingTop: "5px" }} src={brain} alt="logo"></img>
+          <img style={{paddingTop: '5px'}} alt='logo' src={brain}/>
         </div>
       </Tilt>
     </div>
   );
-};
+}
 
 export default Logo;
